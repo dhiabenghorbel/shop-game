188d4c9691708121ecdbc4e1aab00e7f4b732ab4
dhiabenghorbel <dhiaeddinebenghorbel@gmail.com>
2025-01-05 20:03:53 +0100

    Add condition to launch Shop scene only if it's not already active

--- src/scenes/ShopButton.js
+++ src/scenes/ShopButton.js
@@ -3,30 +3,34 @@ import Phaser from "phaser";
 
 export default class ShopButton extends Phaser.Scene {
   constructor() {
-    super({ key: "ShopButton" });
+    super({ key: "ShopButton", active: true }); // Active by default
   }
 
   preload() {
-    // Load images
+    // Load shop button image
     this.load.image("shop", "/shop-elements/shop_title.png");
   }
 
   create() {
+    //Add the shop button image
     const shopButton = this.add
       .image(200, 730, "shop")
       .setInteractive({ useHandCursor: true })
       .setDisplaySize(130, 100);
 
-      shopButton.on("pointerover", () => {
-        shopButton.setScale(0.55); 
-      });
+    shopButton.on("pointerover", () => {
+      shopButton.setScale(0.55);
+    });
 
-      shopButton.on("pointerout", () => {
-        shopButton.setScale(0.5); 
-      });
+    shopButton.on("pointerout", () => {
+      shopButton.setScale(0.5);
+    });
 
+    // When the button is clicked, launch the 'Shop' scene
     shopButton.on("pointerdown", () => {
-      this.scene.launch("Shop");
+      if (!this.scene.isActive("Shop")) {
+        this.scene.launch("Shop");
+      }
     });
   }
 }
